e778c1f438c933328a421676313c29837956973d
DavyVan <affqqa@163.com>
2016-08-05 22:14:37 +0800

    add MultiOutputLayer but not finished yet

---
+++ include/caffe/layers/multi_output_layer.hpp
@@ -0,0 +1,40 @@
+#ifndef MULTI_OUTPUT_LAYER_HPP_
+#define MULTI_OUTPUT_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+namespace caffe
+{
+    template <typename Dtype>
+    class MultiOutputLayer : public Layer<Dtype>
+    {
+    public:
+        explicit MultiOutputLayer(const LayerParameter& param)
+            : Layer<Dtype>(param) {}
+        virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top);
+        virtual void Reshape(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top);
+
+        virtual inline const char* type() const { return "MultiOutput"; }
+        virtual inline int ExactNumBottomBlobs() const { return 1; }
+        virtual inline int ExactNumTopBlobs() const { return 1; }
+
+    protected:
+        virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top);
+        virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top);
+        virtual void Backward_cpu(const vector<Blob<Dtype>*>& top, const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+        virtual void Backward_gpu(const vector<Blob<Dtype>*>& top, const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+        int M_;
+        int K_;
+        int N_;
+        bool bias_term_;
+        Blob<Dtype> bias_multiplier_;
+        bool transpose_;  ///< if true, assume transposed weights
+    };
+}   // namespace caffe
+
+#endif  // MULTI_OUTPUT_LAYER_HPP_
